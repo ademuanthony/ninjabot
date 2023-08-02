48c5de3580856e9192746f490656441d9c7ec833
Ademu Anthony <ademuanthony@gmail.com>
2023-08-02 21:10:31 +0100

    rm log

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -77,7 +77,6 @@ func (d *DataFeedSubscription) Subscribe(pair, timeframe string, consumer DataFe
 }
 
 func (d *DataFeedSubscription) Preload(pair, timeframe string, candles []model.Candle) {
-	log.Infof("[SETUP] preloading %d candles for %s-%s", len(candles), pair, timeframe)
 	key := d.feedKey(pair, timeframe)
 	for _, candle := range candles {
 		if !candle.Complete {
